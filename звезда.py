6849d04be614f71204d4e23b83c9c9984d0d4f92
happyfox102 <136226380+happyfox102@users.noreply.github.com>
2023-10-07 21:24:19 +0300

    Add files via upload

---
+++ звезда.py
@@ -0,0 +1,33 @@
+import turtle
+
+def koch(turtle, length, depth):
+    if depth == 0:
+        turtle.forward(length)
+    else:
+        koch(turtle, length / 3, depth - 1)
+        turtle.left(60)
+        koch(turtle, length / 3, depth - 1)
+        turtle.right(120)
+        koch(turtle, length / 3, depth - 1)
+        turtle.left(60)
+        koch(turtle, length / 3, depth - 1)
+
+def snowflake(turtle, length, depth):
+    for i in range(3):
+        koch(turtle, length, depth)
+        turtle.right(120)
+
+window = turtle.Screen()
+window.bgcolor("white")
+
+my_turtle = turtle.Turtle()
+my_turtle.speed(0)  # Наивысшая скорость
+
+# Перемещаем черепаху, чтобы снежинка была центрирована
+my_turtle.penup()
+my_turtle.backward(150)
+my_turtle.pendown()
+
+snowflake(my_turtle, 300, 3)  # Рисуем снежинку Коха глубиной 3
+
+window.mainloop()
